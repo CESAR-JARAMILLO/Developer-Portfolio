21ca40c13ee8941caf9948ea79ae4329f6273f60
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-03-28 17:46:43 -0700

    created Hero component & added content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Hero from './components/Hero'
+
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import { Box, Container, Image, Link } from '@chakra-ui/react'
+import cesar from '../assets/cesar.png'
+
+const Hero = () => {
+  return (
+    <Container>
+      <Image src={cesar} />
+      <Box>
+        <h1>Nice to meet you! I'm Cesar.</h1>
+
+        <p>Based in Los Angeles, CA. I'm a full-stack developer passionate about building accessible web apps that users love.</p>
+
+        <Box>
+          <Link>Contact Me</Link>
+          <Link>Download Resume</Link>
+        </Box>
+      </Box>
+    </Container>
+  )
+}
+
+export default Hero
