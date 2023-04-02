763188663dba78b898d1efa2cbbf93e278291612
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-02 12:40:52 -0700

    styled for mobile design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,31 @@
-import { Box, Container, Image, Link } from '@chakra-ui/react'
+import { Box, Container, Image, Link, Heading, Text, Stack, Center } from '@chakra-ui/react'
 import cesar from '../assets/cesar.png'
 
 const Hero = () => {
   return (
     <Container>
-      <Image src={cesar} />
-      <Box>
-        <h1>Nice to meet you! I'm Cesar.</h1>
+      <Box position="relative">
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          backgroundColor="rgba(0, 0, 0, 0.5)"
+        />
 
-        <p>Based in Los Angeles, CA. I'm a full-stack developer passionate about building accessible web apps that users love.</p>
+        <Image src={cesar} />
+      </Box>
 
-        <Box>
+      <Box color="white" padding="8" textAlign="center">
+        <Heading size="2xl" pb="6">Nice to meet you! I'm Cesar.</Heading>
+
+        <Text pb='6'>Based in Los Angeles, CA. I'm a full-stack developer passionate about building accessible web apps that users love.</Text>
+
+        <Stack direction="column" spacing="2">
           <Link>Contact Me</Link>
           <Link>Download Resume</Link>
-        </Box>
+        </Stack>
       </Box>
     </Container>
   )
