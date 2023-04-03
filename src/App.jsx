88d13f615895387f5f3d7a7a350ebeed170c8712
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-02 17:26:09 -0700

    added Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import Hero from "./components/Hero";
 import Experience from './components/Experience'
+import Projects from './components/Projects'
 
 function App() {
   return (
     <Box bg="black">
       <Hero />
       <Experience />
+      <Projects />
     </Box>
   );
 }
