b50b663f3046d72c141546eff41129dc85c184cd
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-02 17:26:32 -0700

    created projects data array

---
+++ src/data/projects.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget justo quis nibh blandit semper. Mauris nec tortor est.",
+    image: "/images/project1.png",
+    link: "https://example.com/project1",
+    tags: ["React", "Node.js", "HTML", "CSS", "Database"]
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget justo quis nibh blandit semper. Mauris nec tortor est.",
+    image: "/images/project2.png",
+    link: "https://example.com/project2",
+    tags: ["React", "Node.js", "CSS"]
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget justo quis nibh blandit semper. Mauris nec tortor est.",
+    image: "/images/project3.png",
+    link: "https://example.com/project3",
+    tags: ["React", "Node.js", "HTML", "CSS"]
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget justo quis nibh blandit semper. Mauris nec tortor est.",
+    image: "/images/project4.png",
+    link: "https://example.com/project4",
+    tags: ["React", "Node.js", "HTML"]
+  }
+];
