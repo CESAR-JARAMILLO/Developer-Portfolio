15fcf6927ee1153b539a06f540c121791ed21cf5
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-05 13:18:48 -0700

    fixed heading placement

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,17 @@
-import { Grid, Heading } from '@chakra-ui/react'
+import { Grid, Heading, Box } from '@chakra-ui/react'
 import ProjectCard from './ProjectCard'
 import { projects } from '../data/projects'
 
 const Projects = () => {
   return (
-    <Grid templateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap={4} m={4} justifyItems="center">
+    <Box>
       <Heading textAlign="center" color="white" mb={4}>Some of My Projects</Heading>
+      <Grid templateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap={4} m={4} justifyItems="center">
       {projects.map(project => (
         <ProjectCard key={project.id} project={project} />
       ))}
     </Grid>
+    </Box>
   )
 }
 
