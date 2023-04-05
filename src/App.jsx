25e90587875b60e12c10fe3484e9fd3b0bf65a08
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-05 00:03:26 -0700

    created and styled Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Box } from "@chakra-ui/react";
 import Hero from "./components/Hero";
 import Experience from './components/Experience'
 import Projects from './components/Projects'
+import Header from './components/Header'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <Box bg="black">
+      <Header />
       <Hero />
       <Experience />
       <Projects />
