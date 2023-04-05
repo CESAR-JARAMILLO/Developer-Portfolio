766d18a619fa64d6d918f1ad4b295767a96bafa3
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-04 23:48:58 -0700

    added and styled content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Box, Container, Text, Link } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Box bg="gray.800" color="white" py="6">
+      <Container display="flex" alignItems="center" justifyContent="space-between">
+        <Text>&copy; 2023 Cesar Jaramillo</Text>
+        <Box>
+          <Link mx="2" href="https://www.linkedin.com/in/cesarjara/" target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </Link>
+          <Link mx="2" href="https://github.com/cesarjara" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </Link>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
 
-export default Footer
+export default Footer;
