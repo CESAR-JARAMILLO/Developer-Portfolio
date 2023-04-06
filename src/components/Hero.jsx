e776d9c3662939bcf8419f8b8e5d96f0a9834cb3
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-04-05 18:06:20 -0700

    updated styling for responsive screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,41 @@
-import { Box, Container, Image, Link, Heading, Text, Stack, Center } from '@chakra-ui/react'
+import { Box, Container, Image, Link, Heading, Text, Stack, Center, useBreakpointValue } from '@chakra-ui/react'
 import cesar from '../assets/cesar.png'
 
 const Hero = () => {
+  const flexDirection = useBreakpointValue({ base: "column", md: "row-reverse" });
+  const headingSize = useBreakpointValue({ base: "2xl", md: "3xl" });
+  const textSize = useBreakpointValue({ base: "md", md: "lg" });
+  const spacing = useBreakpointValue({ base: 4, md: 8 });
+
   return (
-    <Container>
-      <Box position="relative">
-        <Box
-          position="absolute"
-          top="0"
-          left="0"
-          width="100%"
-          height="100%"
-          backgroundColor="rgba(0, 0, 0, 0.5)"
-        />
+    <Container maxW="container.xl" px={0}>
+      <Box display="flex" flexDirection={flexDirection} m="4">
+        <Box flex="1" position="relative">
+          <Box
+            position="absolute"
+            top="0"
+            left="0"
+            width="100%"
+            height="100%"
+            backgroundColor="rgba(0, 0, 0, 0.5)"
+          />
 
-        <Image src={cesar} />
-      </Box>
+          <Image src={cesar} borderRadius="full" />
 
-      <Box color="white" padding="8" textAlign="center">
-        <Heading size="2xl" pb="6">Nice to meet you! I'm Cesar.</Heading>
+        </Box>
 
-        <Text pb='6'>Based in Los Angeles, CA. I'm a full-stack developer passionate about building accessible web apps that users love.</Text>
+        <Box flex="1" color="white" padding="8" textAlign="center" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+          <Box>
+            <Heading size={headingSize} pb="6">Nice to meet you! I'm Cesar.</Heading>
 
-        <Stack direction="column" spacing="2">
-          <Link>Contact Me</Link>
-          <Link>Download Resume</Link>
-        </Stack>
+            <Text pb='6' fontSize={textSize}>Based in Los Angeles, CA. I'm a full-stack developer passionate about building accessible web apps that users love.</Text>
+
+            <Stack direction={flexDirection === "column" ? "column" : "row"} spacing={spacing} justifyContent="center">
+              <Link>Contact Me</Link>
+              <Link>Download Resume</Link>
+            </Stack>
+          </Box>
+        </Box>
       </Box>
     </Container>
   )
